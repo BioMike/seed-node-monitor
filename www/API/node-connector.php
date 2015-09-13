2ae8a481203eb730480777bb765605040b41e9a4
Myckel Habets <myckel@sdf.lonestar.org>
2015-09-13 15:39:15 +0200

    Working PHP script

--- www/API/node-connector.php
+++ www/API/node-connector.php
@@ -1,15 +1,18 @@
 <?php
-// $msg contains a real encrypted message. Let's see if we can decrypt it in PHP
-$msg = "empty";
+// $msg contains a real encrypted message. $iv is the IV that belongs with it. Let's see if we can decrypt it in PHP
+$iv = "vCCgcZvaTguiMdq0SVVa4w==";
+$msg = "eFxHUokk2GXosM2pgEcb0ng0TXafEdxRouN5t8e3vWwCuLpwJ1A13qoIgC0uJU6j1qYrLRT3MYpi1KxI7PoxXy7oNWxUlWyvwe8QK5AhEoIkztIShbuBC3OUwtCknIo0/+xc77kM7WFvDX4iFwnP3w==";
 
 $password = "pre-Shared secret abcdefghijklmn";
 $key = mb_convert_encoding($password, "UTF-8");
 
-$data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $msg, MCRYPT_MODE_CBC [, string $iv ]
+// MCRYPT_RIJNDAEL_128, we use a 16 bit key.
+$json_data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($msg), MCRYPT_MODE_CBC, base64_decode($iv));
 
-//key = bytes(secret, 'utf-8')
-//iv = Random.new().read(AES.block_size)
-//cipher = AES.new(key, AES.MODE_CFB, iv)
-//msg = iv + cipher.encrypt(bytes(json_data, 'utf-8'))
+$data = json_decode(ltrim($json_data));
+
+echo "\n";
+print_r($data);
+echo "\n";
 
 ?>
