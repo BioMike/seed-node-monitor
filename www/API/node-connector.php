da01c657d1fe42388bc7ec6a53a4114b5e59b889
Myckel Habets <myckel@sdf.lonestar.org>
2015-09-14 22:46:14 +0200

    Make json_decode return an array.

--- www/API/node-connector.php
+++ www/API/node-connector.php
@@ -25,7 +25,7 @@ $key = mb_convert_encoding($password, "UTF-8");
 // MCRYPT_RIJNDAEL_128, we use a 16 bit key.
 $json_data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($msg), MCRYPT_MODE_CBC, base64_decode($iv));
 
-$data = json_decode(ltrim($json_data));
+$data = json_decode(ltrim($json_data), true);
 
 $db->update_node($ip_address, $data['blocks'], $data['connections'], $data['difficulty'], $data['$nethashrate']);
 
