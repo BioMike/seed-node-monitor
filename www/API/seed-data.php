091e3ccff8c739832116b868a3ce46b95d5bea9a
Myckel Habets <myckel@sdf.lonestar.org>
2015-09-17 17:50:55 +0200

    hook database to seed-data API

--- www/API/seed-data.php
+++ www/API/seed-data.php
@@ -1,16 +1,25 @@
 <?php
+include("database.php");
 
-$conn1 = rand(1,200);
-$conn2 = rand(1,200);
-$conn3 = rand(1,200);
+// Settings
+// $location is where the database is stored. Store it outside the document tree.
+$location = './seeds/';
 
-$array_node_1 = array("name" => "Luxembough", "blocks" => "2551234", "difficulty" => "1234567891234", "connections" => $conn1, "nethashrate" => "1234567891234");
-$array_node_2 = array("name" => "Iceland 1", "blocks" => "2551234", "difficulty" => "1234567891234", "connections" => $conn2, "nethashrate" => "1234567891234");
-$array_node_3 = array("name" => "Iceland 2", "blocks" => "2551234", "difficulty" => "1234567891234", "connections" => $conn3, "nethashrate" => "1234567891234");
+$db = new Database($location);
 
-$data[] = $array_node_1;
-$data[] = $array_node_2;
-$data[] = $array_node_3;
+//$conn1 = rand(1,200);
+//$conn2 = rand(1,200);
+//$conn3 = rand(1,200);
+
+//$array_node_1 = array("name" => "Luxembough", "blocks" => "2551234", "difficulty" => "1234567891234", "connections" => $conn1, "nethashrate" => "1234567891234");
+//$array_node_2 = array("name" => "Iceland 1", "blocks" => "2551234", "difficulty" => "1234567891234", "connections" => $conn2, "nethashrate" => "1234567891234");
+//$array_node_3 = array("name" => "Iceland 2", "blocks" => "2551234", "difficulty" => "1234567891234", "connections" => $conn3, "nethashrate" => "1234567891234");
+
+//$data[] = $array_node_1;
+//$data[] = $array_node_2;
+//$data[] = $array_node_3;
+
+$data = $db->get_seeds_data();
 
 $json_data = json_encode($data);
 echo($json_data);
