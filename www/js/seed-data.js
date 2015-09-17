9f11484d8ba900f573605a493d8b857018afd208
Myckel Habets <myckel@sdf.lonestar.org>
2015-09-17 13:34:29 +0200

    Working javascript

--- www/js/seed-data.js
+++ www/js/seed-data.js
@@ -1,24 +1,55 @@
 function init() {
+ $(document).ready(function() {
+   reload_data();
+   var timerId = setInterval(function() {
+	reload_data();
+   }, 5000);
+});
 
-    $.get( "/API/seed-data.php", function( data ) {
-	for(var i = 0; i < data.length, i++)
-	    {
-	    var node = data[i];
-	    var node_element = document.createElement("div");
-	    // name
-	    var name_element = document.createElement("div");
-	    name_element.appendChild(document.createTextNode(node["name"]));
-	    node_element.appendChild(name_element);
-	    // blocks
-	    var blocks_element = document.createElement("div");
-	    blocks_element.appendChild(document.createTextNode(node["blocks"]));
-	    node_element.appendChild(blocks_element);
-	    // conn
-	    var conn_element = document.createElement("div");
-	    conn_element.appendChild(document.createTextNode(node["connections"]));
-	    node_element.appendChild(conn_element);
-	    // diff
-	    // nethashrate
-	    document.getElementById('nodelist').appendChild(node_element);
-	    }
 };
+
+function reload_data() {
+    $.ajaxSetup({ cache: false });
+    $.get( "/API/seed-data.php", function( data ) {
+        var nodelist = $( "#nodelist" );
+        nodelist.empty();
+	$(jQuery.parseJSON(data)).each(function() {
+	    
+	    var name = this.name;
+	    var blocks = this.blocks;
+	    var conn = this.connections;
+	    var diff = this.difficulty;
+	    var nhr = this.nethashrate;
+
+	    var node_element = $( "<div />" );
+	    node_element.addClass("node-entry");
+	    // name
+	    var name_element = $( "<div />" );
+	    name_element.addClass("node-name");
+	    name_element.append(name);
+	    node_element.append(name_element);
+	    // blocks
+	    var blocks_element = $( "<div />" );
+	    blocks_element.addClass("node-blocks");
+	    blocks_element.append(blocks);
+	    node_element.append(blocks_element);
+	    // conn
+	    var conn_element = $( "<div />" );
+	    conn_element.addClass("node-conn");
+	    conn_element.append(conn);
+	    node_element.append(conn_element);
+	    // diff
+	    var diff_element = $( "<div />" );
+	    diff_element.addClass("node-diff");
+	    diff_element.append(diff);
+	    node_element.append(diff_element);
+	    // nethashrate
+	    var nhr_element = $( "<div />" );
+	    nhr_element.addClass("node-conn");
+	    nhr_element.append(nhr);
+	    node_element.append(nhr_element);
+
+	    nodelist.append(node_element);
+	    });
+	});
+    };
