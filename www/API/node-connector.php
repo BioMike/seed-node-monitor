b37699e395bacd574cba613e6180252616a3eb3c
Myckel Habets <myckel@sdf.lonestar.org>
2015-09-22 23:14:41 +0200

    Secure API input a bit

--- www/API/node-connector.php
+++ www/API/node-connector.php
@@ -11,8 +11,8 @@ $db = new Database($location);
 $ip_address = $_SERVER['REMOTE_ADDR'];
 
 // Get the data
-$iv = $_POST['iv'];
-$msg = $_POST['msg'];
+$iv_post = $_POST['iv'];
+$msg_post = $_POST['msg'];
 
 $password = $db->get_password($ip_address);
 if(!$password)
@@ -22,11 +22,17 @@ if(!$password)
     }
 $key = mb_convert_encoding($password, "UTF-8");
 
-// MCRYPT_RIJNDAEL_128, we use a 16 bit key.
-$json_data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($msg), MCRYPT_MODE_CBC, base64_decode($iv));
+$iv = base64_decode($iv_post, true);
+$msg = base64_decode($msg_post, true);
 
-$data = json_decode(ltrim($json_data), true);
+if($iv && $msg)
+    {
+    // MCRYPT_RIJNDAEL_128, we use a 16 bit key.
+    $json_data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $msg, MCRYPT_MODE_CBC, $iv);
 
-$db->update_node($ip_address, $data['blocks'], $data['connections'], $data['difficulty'], $data['nethashrate']);
+    $data = json_decode(ltrim($json_data), true);
+
+    $db->update_node($ip_address, $data['blocks'], $data['connections'], $data['difficulty'], $data['nethashrate']);
+    }
 
 ?>
